441e2ae832dd39924323977aa4c53f83528bacbc
achujelas123 <achujel3@gmail.com>
2017-04-19 16:20:29 +0300

    first commit

---
+++ src/main/java/org/springTest/main/AopMain.java
@@ -0,0 +1,17 @@
+package org.springTest.main;
+
+import org.springTest.services.ShapeService;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+public class AopMain {
+
+    public static void main(String[] args) {
+
+        ApplicationContext applicationContext = new ClassPathXmlApplicationContext("spring.xml");
+        ShapeService shapeService = applicationContext.getBean("shapeService", ShapeService.class);
+        System.out.println(shapeService.getTriangle().getName());
+
+    }
+
+}
