fb6110c9dc8b4e69b3bab25e5fa824df1afe12d8
achujelas123 <achujel3@gmail.com>
2017-04-19 18:05:15 +0300

    testing pointcut and wildcard expressions

--- src/main/java/org/springtest/aspects/LoggingAspect.java
+++ src/main/java/org/springtest/aspects/LoggingAspect.java
@@ -2,13 +2,23 @@ package org.springtest.aspects;
 
 import org.aspectj.lang.annotation.Aspect;
 import org.aspectj.lang.annotation.Before;
+import org.aspectj.lang.annotation.Pointcut;
 
 @Aspect
 public class LoggingAspect {
 
-    @Before("execution(* *())")
+    @Before("allGetters()")
     public void loggingAdvice() {
         System.out.println("Advice run. Get method called");
     }
 
+    @Before("allGetters()")
+    public void secondAdvice() {
+        System.out.println("Second advice executed");
+    }
+
+    @Pointcut("execution(* org.springtest.model.*.get*())")
+    public void allGetters(){
+
+    }
 }
